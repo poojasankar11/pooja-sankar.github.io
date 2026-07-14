f20aa0f98f7956af172ecb4a3ab28e69f6afd835
poojasankar11 <pooja.sankar11@gmail.com>
2026-07-13 22:58:52 -0700

    Update profile.js

--- profile.js
+++ profile.js
@@ -25,7 +25,7 @@ const studentProfile = {
 
   If you do not want to use a photo, leave this as an empty string.
   */
-  photo: "profile.jpg",
+  photo: "profile.jpeg",
 
   contact: "Contact information available upon request.",
 
